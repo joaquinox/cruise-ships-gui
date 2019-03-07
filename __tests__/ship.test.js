3d1ed5779e40956544ed75e61b5b8c24e8c5d3e6
Sam Jones <samueldjones24@gmail.com>
2019-03-07 20:13:39 +0000

    setSail function added

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,12 +2,20 @@
 const Ship = require('../src/ship.js');
 
 describe('Ship', () => {
-    it('can be instantiated', () => {
-        expect(new Ship()).toBeInstanceOf(Object);
-    });
+it('can be instantiated', () => {
+    expect(new Ship()).toBeInstanceOf(Object);
+  });
 
-    it('has a starting port', () => {
-        const ship = new Ship('Dover');
-        expect(ship.startingPort).toBe('Dover');
-    });
+it('has a starting port', () => {
+    const ship = new Ship('Dover');
+    expect(ship.startingPort).toBe('Dover');
+  });
+});
+
+describe('setSail', () => {
+it('ship can set sail', () => {
+    const ship = new Ship('Dover');
+    ship.setSail();
+    expect(ship.startingPort).toBeFalsy();
+  });
 });
--- src/ship.js
+++ src/ship.js
@@ -2,4 +2,10 @@
 function Ship(startingPort) {
     this.startingPort = startingPort;
 }
+
+Ship.prototype.setSail = function() {
+    this.startingPort = !true;
+    return 'Bon voyage!';
+};
+
 module.exports = Ship;
