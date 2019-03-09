2af9cee7e43e3b293c753f7cf60d919f647172d7
Sam Jones <samueldjones24@gmail.com>
2019-03-09 17:00:08 +0000

    Created addShip/removeShip functions + tests

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -9,4 +9,25 @@ it('Port has a name property', () => {
     const port = new Port('Dover');
     expect(port.name).toBe('Dover');
   });
+
+it('addShip', () => {
+    const port = new Port('Dover');
+    const ship = {};
+
+    port.addShip(ship);
+
+    expect(port.ships).toContain(ship);
+  });
+
+it('removeShip', () => {
+    const port = new Port('Dover');
+    const ship = {};
+    const ship1 = {};
+
+    port.addShip(ship);
+    port.addShip(ship1);
+    port.removeShip(ship1);
+
+    expect(port.ships).toEqual([ship]);
+  });
 });
--- src/port.js
+++ src/port.js
@@ -1,7 +1,15 @@
 
 function Port(name) {
   this.name = name;
+  this.ships = [];
 }
 
+Port.prototype.addShip = function addShip(ship) {
+  this.ships.push(ship);
+};
+
+Port.prototype.removeShip = function removeShip(ship) {
+  this.ships.splice(this.ships.indexOf[ship], 1);
+};
 
 module.exports = Port;
