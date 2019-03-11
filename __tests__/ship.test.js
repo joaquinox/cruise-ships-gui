d55b640b0a7f9430fe71706b84e3318809457a43
Sam Jones <samueldjones24@gmail.com>
2019-03-11 20:41:30 +0000

    Renamed constants to be more readable

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,6 +1,7 @@
 /* globals describe it expect */
 const Ship = require('../src/ship.js');
 const Port = require('../src/port.js');
+const Itinerary = require('../src/itinerary.js');
 
 
 describe('Ship', () => {
@@ -9,16 +10,24 @@ it('can be instantiated', () => {
   });
 
 it('has a starting port', () => {
-    const port = new Port('Dover');
-    const ship = new Ship(port);
-    expect(ship.currentPort).toBe(port);
+    const dover = new Port('Dover');
+    const ship = new Ship(dover);
+    expect(ship.currentPort).toBe(dover);
+  });
+
+it('gets added to port on instantiation', () => {
+    const dover = new Port('Dover');
+    const itinerary = new Itinerary([dover]);
+    const ship = new Ship(itinerary);
+
+    expect(dover.ships).toContain(ship);
   });
 });
 
 describe('setSail', () => {
 it('ship can set sail', () => {
-    const port = new Port('Dover');
-    const ship = new Ship(port);
+    const dover = new Port('Dover');
+    const ship = new Ship(dover);
     ship.setSail();
     expect(ship.currentPort).toBeFalsy();
   });
--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,11 @@
 
 function Ship(port) {
   this.currentPort = port;
+  // this.currentPort.addShip(this);
 }
 
 Ship.prototype.setSail = function setSail() {
-  this.currentPort = false;
+  this.currentPort = !true;
 };
 
 Ship.prototype.dock = function dock(port) {
