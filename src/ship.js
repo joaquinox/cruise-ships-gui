0af68a88006df5260f671b08ed3e30833753506e
Sam Jones <samueldjones24@gmail.com>
2019-03-12 22:26:16 +0000

    Modified constructor + prototypes so that ports now possess ships

--- src/ship.js
+++ src/ship.js
@@ -1,15 +1,22 @@
 
-function Ship(port) {
-  this.currentPort = port;
-  // this.currentPort.addShip(this);
+function Ship(itinerary) {
+  this.itinerary = itinerary;
+  this.currentPort = itinerary.ports[0];
+  this.previousPort = null;
+  this.currentPort.addShip(this);
 }
 
 Ship.prototype.setSail = function setSail() {
-  this.currentPort = !true;
+  this.previousPort = this.itinerary.ports[0];
+  this.currentPort = false;
+  this.previousPort.removeShip(this);
 };
 
-Ship.prototype.dock = function dock(port) {
-  this.currentPort = port;
+Ship.prototype.dock = function dock() {
+  const itinerary = this.itinerary;
+  const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+  this.currentPort = itinerary.ports[previousPortIndex + 1];
+  this.currentPort.addShip(this);
 };
 
 module.exports = Ship;
