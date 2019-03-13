1cc455158cb3d94e51b566484d96658a37773d90
Sam Jones <samueldjones24@gmail.com>
2019-03-13 21:35:42 +0000

    DRY up ship test suite

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -18,49 +18,33 @@ describe('Ship', () => {
   });
 
 it('can be instantiated', () => {
-    // const dover = new Port('Dover');
-    // const itinerary = new Itinerary([dover]);
     expect(new Ship(itinerary)).toBeInstanceOf(Object);
   });
 
 it('has a starting port', () => {
-    // const dover = new Port('Dover');
-    // const ship = new Ship(dover);
     expect(ship.currentPort).toEqual(dover);
   });
 
 it('gets added to port on instantiation', () => {
-    // const dover = new Port('Dover');
-    // const itinerary = new Itinerary([dover]);
-    // const ship = new Ship(itinerary);
-
     expect(dover.ships).toContain(ship);
   });
 
-// describe('setSail', () => {
+describe('setSail', () => {
 it('ship can set sail', () => {
-    // const dover = new Port('Dover');
-    // const calais = new Port('Calais');
-    // const itinerary = new Itinerary([dover, calais]);
-    // const ship = new Ship(itinerary);
+      ship.setSail();
 
-    ship.setSail();
+      expect(ship.currentPort).toBeFalsy();
+      expect(dover.ships).not.toContain(ship);
+    });
 
-    expect(ship.currentPort).toBeFalsy();
-    expect(dover.ships).not.toContain(ship);
-  });
-
-// describe('dock', () => {
+describe('dock', () => {
 it('ship can dock at a different port', () => {
-    // const dover = new Port('Dover');
-    // const calais = new Port('Calais');
-    // const itinerary = new Itinerary([dover, calais]);
-    // const ship = new Ship(itinerary);
+        ship.setSail();
+        ship.dock();
 
-    ship.setSail();
-    ship.dock();
-
-    expect(ship.currentPort).toEqual(calais);
-    expect(calais.ships).toContain(ship);
+        expect(ship.currentPort).toEqual(calais);
+        expect(calais.ships).toContain(ship);
+      });
+    });
   });
 });
