b4c756262b583f387d21241a92f3396029c8d8bd
Sam Jones <samueldjones24@gmail.com>
2019-03-13 21:40:09 +0000

    DRY up port test suite

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,29 +1,37 @@
 const Port = require('../src/port.js');
+const Ship = require('../src/ship.js');
+const Itinerary = require('../src/itinerary.js');
 
 describe('Port', () => {
+  let ship;
+  let ship1;
+  let dover;
+  let calais;
+  let itinerary;
+
+  beforeEach(() => {
+    dover = new Port('Dover');
+    calais = new Port('Calais');
+    itinerary = new Itinerary([dover, calais]);
+    ship = { name: 'ship 0' };
+    ship1 = { name: 'ship 1' };
+  });
+
 it('can be instantiated', () => {
     expect(new Port()).toBeInstanceOf(Object);
   });
 
 it('Port has a name property', () => {
-    const dover = new Port('Dover');
     expect(dover.name).toBe('Dover');
   });
 
 it('addShip', () => {
-    const dover = new Port('Dover');
-    const ship = {};
-
     dover.addShip(ship);
 
     expect(dover.ships).toContain(ship);
   });
 
 it('removeShip', () => {
-    const dover = new Port('Dover');
-    const ship = { name: 'ship 0' };
-    const ship1 = { name: 'ship 1' };
-
     dover.addShip(ship);
     dover.addShip(ship1);
     dover.removeShip(ship1);
