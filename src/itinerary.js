f6fba8bf540d3c3331959be07d875d31acd970b1
Sam Jones <samueldjones24@gmail.com>
2019-04-01 21:11:48 +0100

    Applied IIFE to constructor files

--- src/itinerary.js
+++ src/itinerary.js
@@ -1,6 +1,11 @@
 
-function Itinerary(ports) {
-  this.ports = ports;
-}
-
-module.exports = Itinerary;
+(function exportItinerary() {
+  function Itinerary(ports) {
+    this.ports = ports;
+  }
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Itinerary;
+  } else {
+    window.Port = Itinerary;
+  }
+}());
--- src/ship.js
+++ src/ship.js
@@ -1,22 +1,30 @@
 
-function Ship(itinerary) {
-  this.itinerary = itinerary;
-  this.currentPort = itinerary.ports[0];
-  this.previousPort = null;
-  this.currentPort.addShip(this);
-}
+(function exportShipfunction() {
+  function Ship(itinerary) {
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
+    this.previousPort = null;
+    this.currentPort.addShip(this);
+  }
 
-Ship.prototype.setSail = function setSail() {
-  this.previousPort = this.itinerary.ports[0];
-  this.currentPort = false;
-  this.previousPort.removeShip(this);
-};
+  Ship.prototype = {
+    setSail() {
+      this.previousPort = this.itinerary.ports[0];
+      this.currentPort = false;
+      this.previousPort.removeShip(this);
+    },
 
-Ship.prototype.dock = function dock() {
-  const itinerary = this.itinerary;
-  const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
-  this.currentPort = itinerary.ports[previousPortIndex + 1];
-  this.currentPort.addShip(this);
-};
+    dock() {
+      const itinerary = this.itinerary;
+      const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+      this.currentPort = itinerary.ports[previousPortIndex + 1];
+      this.currentPort.addShip(this);
+    },
+  };
 
-module.exports = Ship;
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Ship;
+  } else {
+    window.Port = Ship;
+  }
+}());
